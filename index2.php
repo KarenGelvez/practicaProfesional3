808e75ab85ff52c4051001271f2260cbdec7219d
KarenGelvez <karen.gelvez2@unipamplona.edu.co>
2020-09-19 07:13:16 -0500

    primeros archivos

---
+++ index2.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Formulario</title>
+</head>
+<body>
+<form method="POST" action='sumar.php'>
+<p> Valor 1: <input type='text' name='T1' size='20'></p>
+<p> Valor 2: <input type='text' name='T2' size='20'></p>
+<p> Valor 3: <input type='text' name='T3' size='20'></p>
+<p> Valor 1: <input type='submit' name='B1' value='sumar'></p>
+</form>
+</body>
+</html>
