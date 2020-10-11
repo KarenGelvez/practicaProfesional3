d436d7d5c42d2b434082f7270e5126b8fbe7c809
Karen Gelvez <61369254+KarenGelvez@users.noreply.github.com>
2020-10-11 10:34:45 -0500

    Update index2.php
    Modificando una etiqueta

--- index2.php
+++ index2.php
@@ -7,7 +7,7 @@
 <p> Valor 1: <input type='text' name='T1' size='20'></p>
 <p> Valor 2: <input type='text' name='T2' size='20'></p>
 <p> Valor 3: <input type='text' name='T3' size='20'></p>
-<p> Valor 1: <input type='submit' name='B1' value='sumar'></p>
+<p><input type='submit' name='B1' value='sumar'></p>
 </form>
 </body>
 </html>
