c10e5b707b1383ef02b1e623bd232dc34ac9adb5
Karen Gelvez <61369254+KarenGelvez@users.noreply.github.com>
2020-10-11 17:52:53 -0500

    Create exampleFuntion.py

---
+++ exampleFuntion.py
@@ -0,0 +1,10 @@
+import so
+def lambda_handler(evento, context):
+    what_to_print = so.environ.get('what_to_print')
+    how_many_times = int(os.environ.get('how_many_times'))
+
+if what_to_print and how_many_times > 0:
+    for i in range(0, how_many_times):
+        print(f"what_to_print: {what_to_print}")
+    return what_to_print
+return None
