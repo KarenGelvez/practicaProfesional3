536aa710ebcc5984c88aa11bcfdac479351ab78b
Karen Gelvez <61369254+KarenGelvez@users.noreply.github.com>
2020-10-12 12:13:56 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Demo Example</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="jumbotron">
+            <div class="container">
+                <h1>Welcome!</h1>
+                <h3>Demo Example Elastic Beanstalk</h3>
+                <p>Application PHP</p>
+
+            </div>
+        </div>
+    </body>
+</html>
